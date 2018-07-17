c24eda997b6e2e16352e1ba9cba27d4c031abceb
Mariusz <zychmarius@gmail.com>
2018-07-17 19:19:37 +0200

    add service connection

--- app/src/main/java/project/own/hashratemonitor/MainActivity.java
+++ app/src/main/java/project/own/hashratemonitor/MainActivity.java
@@ -1,14 +1,17 @@
 package project.own.hashratemonitor;
 
-import android.app.ProgressDialog;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import org.json.JSONObject;
@@ -20,6 +23,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,106 +33,87 @@ public class MainActivity extends AppCompatActivity {
     private Button settingsMain;
     private TextView hashrateField;
     private TextView infoStatusText ;
+    private WorkService workService;
 
+
+    private ServiceConnection serviceConnection =new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder service) {
+            WorkService.MyBinder binder = (WorkService.MyBinder) service;
+            workService = binder.getworkService();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            workService = null;
+
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Do something after 100ms
+                showHashrate();
+                handler.postDelayed(this, 5000);
+            }
+        }, 1500);
+
+
     }
 
     public void clickStartButton(View view){
         infoStatusText = (TextView)findViewById(R.id.infoStatusText);
+        startBackgroundWorkService();
 
-        new WebServicesHandler()
-                .execute("https://ethereum.miningpoolhub.com/index.php?page=api&action=getuserhashrate&api_key=userkey&id=userid");
         infoStatusText.setText("sledzenie w toku aby wtrzymać kliknij stop");
+
     }
+
+
     public void clickStopButton(View view){
+        stopBackgroundWorkService();
         infoStatusText = (TextView)findViewById(R.id.infoStatusText);
         infoStatusText.setText("sledzenie wsztrzymane aby rozpocząć kliknij start");
+    }
+
+    private void startBackgroundWorkService() {
+        Intent serviceIntent = new Intent(this, WorkService.class);
+        startService(serviceIntent);
+        bindService(serviceIntent, serviceConnection, Context.BIND_AUTO_CREATE);
 
     }
+
+    private void showHashrate() {
+        if(workService != null){
+            ((TextView) findViewById(R.id.hashrateField)).setText(workService.showHashrate());
+        }
+    }
+
+    private void stopBackgroundWorkService() {
+        Intent serviceIntent = new Intent(this, WorkService.class);
+        unbindService(serviceConnection);
+        stopService(serviceIntent);
+    }
+
     public void clickSettingsButton(View view){
         Intent openSettingsScreen = new Intent(getApplicationContext(),Settings.class);
         startActivity(openSettingsScreen);
     }
-    private class WebServicesHandler extends AsyncTask<String, Void, String>{
-        private ProgressDialog dialog = new ProgressDialog(MainActivity.this);
 
+    private class MyTimerTask extends TimerTask {
         @Override
-        protected void onPreExecute(){
-            dialog.setMessage("Czekaj...");
-            dialog.show();
-        }
-
-        @Override
-        protected String doInBackground(String... urls){
-
-            try{
-                URL url = new URL(urls[0]);
-                URLConnection connection = url.openConnection();
-
-                InputStream in = new BufferedInputStream(connection.getInputStream());
-
-                return streamToString(in);
-            }catch (Exception e){
-                Log.d(MainActivity.class.getSimpleName(), e.toString());
-                return null;
-            }
-        }
-
-        @Override
-        protected void onPostExecute(String result){
-
-            dialog.dismiss();
-
-            try {
-
-//                ((TextView) findViewById(R.id.test1)).setText(result);
-                JSONObject json = new JSONObject(result);
-                JSONObject getuserhashrate = json.getJSONObject("getuserhashrate");
-                String hashrate = getuserhashrate.getString("data");
-                int i =0;
-                while (hashrate.charAt(i)!='.'){
-                    i++;
-                }
-                if(i<2){
-                    ((TextView) findViewById(R.id.hashrateField)).setText("0");
-                }else if(i<3){
-                    String endHashrate = hashrate.substring(0,i);
-                    ((TextView) findViewById(R.id.hashrateField)).setText("0.0"+endHashrate);
-                }else if(i<4){
-                    String endHashrate = hashrate.substring(0,i);
-                    ((TextView) findViewById(R.id.hashrateField)).setText("0."+endHashrate);
-                }else {
-                    String endHashrate = hashrate.substring(0,i-3);
-                    ((TextView) findViewById(R.id.hashrateField)).setText(endHashrate);
-                }
-
-            }catch (Exception e) {
-                Log.d(MainActivity.class.getSimpleName(), e.toString());
-            }
+        public void run() {
+            showHashrate();
         }
     }
-    public static String streamToString(InputStream is) {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-        StringBuilder stringBuilder = new StringBuilder();
-        String line = null;
-
-        try {
-
-            while ((line = reader.readLine()) != null) {
-                stringBuilder.append(line + "\n");
-            }
-
-            reader.close();
-
-        } catch (IOException e) {
-            // obsłuż wyjątek
-            Log.d(MainActivity.class.getSimpleName(), e.toString());
-        }
-
-        return stringBuilder.toString();
-    }
+    
 
 }
