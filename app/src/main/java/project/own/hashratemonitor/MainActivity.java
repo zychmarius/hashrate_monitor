a4d771457e89276ae1d7200e08cbe8d5af25e61a
Mariusz <zychmarius@gmail.com>
2018-11-11 15:11:57 +0100

    add settings

--- app/src/main/java/project/own/hashratemonitor/MainActivity.java
+++ app/src/main/java/project/own/hashratemonitor/MainActivity.java
@@ -4,26 +4,14 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
-import android.os.AsyncTask;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import org.json.JSONObject;
-
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
@@ -32,11 +20,11 @@ public class MainActivity extends AppCompatActivity {
     private Button stopMain;
     private Button settingsMain;
     private TextView hashrateField;
-    private TextView infoStatusText ;
+    private TextView infoStatusText;
     private WorkService workService;
 
 
-    private ServiceConnection serviceConnection =new ServiceConnection() {
+    private ServiceConnection serviceConnection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName componentName, IBinder service) {
             WorkService.MyBinder binder = (WorkService.MyBinder) service;
@@ -49,11 +37,11 @@ public class MainActivity extends AppCompatActivity {
 
         }
     };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
 
         final Handler handler = new Handler();
@@ -69,8 +57,8 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void clickStartButton(View view){
-        infoStatusText = (TextView)findViewById(R.id.infoStatusText);
+    public void clickStartButton(View view) {
+        infoStatusText = (TextView) findViewById(R.id.infoStatusText);
         startBackgroundWorkService();
 
         infoStatusText.setText("sledzenie w toku aby wtrzymać kliknij stop");
@@ -78,9 +66,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void clickStopButton(View view){
+    public void clickStopButton(View view) {
         stopBackgroundWorkService();
-        infoStatusText = (TextView)findViewById(R.id.infoStatusText);
+        infoStatusText = (TextView) findViewById(R.id.infoStatusText);
         infoStatusText.setText("sledzenie wsztrzymane aby rozpocząć kliknij start");
     }
 
@@ -92,7 +80,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void showHashrate() {
-        if(workService != null){
+        if (workService != null) {
             ((TextView) findViewById(R.id.hashrateField)).setText(workService.showHashrate());
         }
     }
@@ -103,8 +91,8 @@ public class MainActivity extends AppCompatActivity {
         stopService(serviceIntent);
     }
 
-    public void clickSettingsButton(View view){
-        Intent openSettingsScreen = new Intent(getApplicationContext(),Settings.class);
+    public void clickSettingsButton(View view) {
+        Intent openSettingsScreen = new Intent(getApplicationContext(), Settings.class);
         startActivity(openSettingsScreen);
     }
 
@@ -114,6 +102,6 @@ public class MainActivity extends AppCompatActivity {
             showHashrate();
         }
     }
-    
+
 
 }
--- app/src/main/java/project/own/hashratemonitor/Settings.java
+++ app/src/main/java/project/own/hashratemonitor/Settings.java
@@ -1,13 +1,61 @@
 package project.own.hashratemonitor;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Settings extends AppCompatActivity {
+
+    public static final String SETTINGS = "application.settings";
+    public static final String HASHRATE_ALARM_VALUE = "hashrate";
+    public static final String ETHEREUM_APIKEY = "ethereumApikey";
+    public static final String USER_ID = "userID";
+    EditText hashrateAlarm;
+    EditText ethApikey;
+    EditText userID;
+    Button setHashrate;
+    Button setApikey;
+    Button setID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        final SharedPreferences preferences = getSharedPreferences(SETTINGS, MODE_PRIVATE);
+        final SharedPreferences.Editor editor = preferences.edit();
+
+        hashrateAlarm = (EditText) findViewById(R.id.hasrateValue);
+        hashrateAlarm.setText(preferences.getString(HASHRATE_ALARM_VALUE, "0"));
+        ethApikey = (EditText) findViewById(R.id.apikeySet);
+        ethApikey.setText(preferences.getString(ETHEREUM_APIKEY, "brak"));
+        userID = (EditText) findViewById(R.id.setID);
+        userID.setText(preferences.getString(USER_ID, "bral"));
+        setHashrate = (Button) findViewById(R.id.setHashrateButton);
+        setHashrate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editor.putString(HASHRATE_ALARM_VALUE, hashrateAlarm.getText().toString());
+                editor.commit();
+            }
+        });
+        setApikey = (Button) findViewById(R.id.apikeySetButton);
+        setApikey.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               editor.putString(ETHEREUM_APIKEY, ethApikey.getText().toString());
+               editor.commit();
+            }
+        });
+        setID = (Button) findViewById(R.id.setIDButton);
+        setID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editor.putString(USER_ID, userID.getText().toString());
+                editor.commit();
+            }
+        });
     }
 }
